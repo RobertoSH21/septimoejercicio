d64cb32f06d47575a2a7c2205ca325c6ee7c4e40
RobertoSH <al217053@alumnos.uacj.mx>
2022-05-02 21:43:48 -0600

    agrengando y probando un post para crear un explorer

--- Express_api/app.js
+++ Express_api/app.js
@@ -5,7 +5,7 @@ app.use(express.json())
 const port = 3000
 
 app.get('/v1/explorers', (req,res) => {
-    console.log('Api Explorers GET ALL requests ${new Date()}')
+    console.log(`Api Explorers GET ALL requests ${new Date()}`)
     const explorer1 = {id: 1, name: "Carlo1"}
     const explorer2 = {id: 2, name: "Carlo2"}
     const explorer3 = {id: 3, name: "Carlo3"}
@@ -21,6 +21,11 @@ app.get('/v1/explorers/:id', (req,res) => {
     res.status(200).json(explorer)
 })
 
+app.post('/v1/explorers', (req,res) => {
+    console.log(`Api Explorers post requests ${new Date()}`)
+    const requestBody = req.body
+    res.status(201).json({message: "Created"})
+})
 
 
 app.listen(port, () => {
